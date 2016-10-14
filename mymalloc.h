1a156130e9294a766ad7f8eb1f800f9b2dc387f9
Sagar Patel <sagar5000@gmail.com>
2016-10-14 17:07:17 -0400

    header

---
+++ mymalloc.h
@@ -0,0 +1,9 @@
+#ifndef __MYMALLOC_FUNCTIONS_H__
+#define __MYMALLOC_FUNCTIONS_H__
+
+
+char *malloc (const int size);
+void defrag();
+int free(const char * ptrFree);
+
+#endif // __MYMALLOC_FUNCTIONS_H__
