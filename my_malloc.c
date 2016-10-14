a504eabf6be3877fdeee5285866dca404ae63ddd
Andrew Khazanovich <akhazanovich+GIT@gmail.com>
2016-10-13 20:12:42 -0400

    Khaz fixing everything

--- my_malloc.c
+++ my_malloc.c
@@ -2,24 +2,85 @@
 
 char array[5000]
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
+//Sagar Patel
+//Brilliance
 
+static char myblock[5000];
+static char *error = "Not Enough Space";
+int first = 1;
+
+void *malloc1 (int size) {
+  if (!intializedArray){
+    *(short*) myblock = 4998 +1;
+    initializedArray=1;
+  }
+  if (size < 0){
+    printf("Must be greater than 0\n");
+    return error;
+  }
+  
+  if(size >4998){
+    printf("Memory request larger then size of heap")
+    return error
+  }
+  if(!size)size=2;
+
+  if (size%2) size++;
+
+  unsigned short short_size= (unsigned short) size;
+  char *curr = myblock;
+  short currBlockSize;
+
+  while(curr < &myblock[4999]){
+  if(*(short *) curr) currBlockSize = *(short*)curr &-1;
+  else{
+    printf("FUBAR\n");
+  }
+
+  if (currBlockSize%2 == 0 && currBlockSize >= short_size){  //check if location is free
+          *(short*)(curr) = short_size+1;   //+1 to signify allocated space 
+          *(short*)(curr+short_size+2) = currBlocksize - short_size-2;
+          return curr+2;
+        }
+        curr+=currBlockSize;
+    }
+  
+  printf("Not Enough Space - 0 signifies unallocated\n");
+  return NULL;
+}
 int free(ptrFree){
+	if(ptrFree < &array[2] || ptrFree>&array[4999]) return (-1);
+	char * tracker = myblock;
+	while(tracker<=(ptrFree-2)){
+		if(tracker==(ptrFree-2) && (*(short *)tracker&1)){
+			*(short*)tracker--;
+			defrag();
+			return 1;
+		}
+		
+		tracker+=*(short*)tracker~1 + 2;
+	}
 	
-	if(ptrFree > &array[2]) short* meta = ptrFree -2;
-	else return -1;
-	if(meta%2==1) *meta -=1;
-	else return -1;
-	degfrag();
-	return 1;
+	return (-1);
+
 }
 
+
 void defrag(){
-	short* meta = array;
-	short size;
-	while((meta + size) < 5000 ){
-		if((*meta%2 == 0) && (*(meta + size)%2==0)) *meta += *(meta + size);
+	short meta_size = *(short*)array;
+	char * tracker = array;
+	short block1Size, block2Size; 
+	while(!(*(short *)tracker&1 && !(*(short *)(tracker + *(short *)tracker~1 +2)&1)({
+		
+		*(short *)tracker+=*(short *)(tracker + *(short *)tracker~1);
+		*(short *)(tracker + )
 		size = *meta - (*meta%2);
 		meta += size;
 		}
 	}
+
--- sagarMalloc.c
+++ sagarMalloc.c
@@ -6,68 +6,52 @@
 //Sagar Patel
 //Brilliance
 
-static char myblock[50];
+static char myblock[5000];
 static char *error = "Not Enough Space";
-int first = 1;
+int initializedArray = 0;
 
-char *malloc1 (int size) {
-
-  if (size == 0){
+void *malloc1 (int size) {
+  if (!intializedArray){
+    *(short*) myblock = 4998 +1;
+    initializedArray=1;
+  }
+  if (size < 0){
     printf("Must be greater than 0\n");
     return error;
   }
-
-  if (size%2 != 0){
-    printf("Only Even Sizes\n");
-    exit(0);
-  }
-
-	char* p;
-  p=myblock;
   
-  unsigned short short_size= size;
+  if(size >4998){
+    printf("Memory request larger then size of heap")
+    return error
+  }
+  if(!size)size=2;
 
-  if (first == 1){
-    *(short*)(p) = short_size;
-    first=0;
-    return p+2;
+  if (size%2) size++;
+
+  unsigned short short_size= (unsigned short) size;
+  char *curr = myblock;
+  short currBlockSize;
+
+  while(curr < &myblock[4999]){
+  if(*(short *) curr) currBlockSize = *(short*)curr &-1;
+  else{
+    printf("FUBAR\n");
   }
 
-  int i =0; char *curr = myblock;
-  for (i=0;i<=50;i++){
-    short val = *(short*)(curr+i);
-    if (val != 0){ //check if location has value
-      if (val%2 != 0){  //check if location is free
-        if (val >= size){ //check if block is large enough
-          *(short*)(curr+i) = short_size; //+1 to signify allocated space 
-          return curr+i+2;
+  if (currBlockSize%2 == 0 && currBlockSize >= short_size){  //check if location is free
+          *(short*)(curr) = short_size+1;   //+1 to signify allocated space 
+          *(short*)(curr+short_size+2) = currBlocksize - short_size-2;
+          return curr+2;
         }
-      }
-      else{
-        i=i+val+2;
-          } 
+        curr+=currBlockSize;
     }
-    else{ //if location does not have value, try to add here
-      char *temp_ptr = curr+i; 
-      int j=0; int run =0;
-      for (j=0;j<val;j++){ //check if space is big enough for allocation
-        short temp_val = *(short*)(temp_ptr+j);
-        if (val != 0){  //if any space has value then move pointer to new location
-          i=i+temp_val;
-          run =1;
-          break;
-      }
-    } //space available for malloc 
-    if (run == 0){
-      *(short*)(curr+i) = short_size;
-      return curr+i+2;
-      }
-    }
-  }
-
+  
   printf("Not Enough Space - 0 signifies unallocated\n");
-  return error;
+  return NULL;
 }
+
+ 
+
 
 
 int main(int argc, char *argv[]) {
