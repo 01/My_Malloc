ffba6d31779210145b9a1c97d2f0539aa34524a2
Andrew Khazanovich <akhazanovich+GIT@gmail.com>
2016-10-14 20:17:21 -0400

    Seems to be working other then test D

--- my_malloc.c
+++ my_malloc.c
@@ -3,45 +3,58 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <time.h>
 
 static char myblock[5000];
 static char *error = "Not Enough Space";
 int first =0;
 
 void defrag();
+
 int free1(void * ptrFree);
 void * malloc1(unsigned int size);
-/*void defrag(){
+void defrag();
+void defrag(){
   
-  short meta_size = *(short*)myblock;
+  
   char * tracker = myblock;
-  short block1Size =*(short *)tracker~- , block2Size; 
-  while(!(*(short *)tracker&1 && !(*(short *)(tracker + *(short *)tracker~1 +2)&1)))
-  {  
-    *(short *)tracker+=*(short *)(tracker + *(short *)tracker~1+2);
-  
+  unsigned short meta_size1 = * (unsigned short *) tracker;
+  unsigned short block_size1 = (meta_size1 - (meta_size1%2));
+  unsigned short meta_size2 = * (unsigned short *) (tracker +2 + block_size1);
+  unsigned short block_size2 = (meta_size2 - (meta_size2%2));
+  while((tracker + 2 + block_size1)< &myblock[4998]){
+  	meta_size1 = * (unsigned short *) tracker;
+  	block_size1 = (meta_size1 - (meta_size1%2));
+  	meta_size2 = * (unsigned short *) (tracker +2 + block_size1);
+  	block_size2 = (meta_size2 - (meta_size2%2));
+  while(!(meta_size1%2) && !(meta_size2) && (tracker + 2 + block_size1)< &myblock[4998]){  
+    *(unsigned short *)tracker += (block_size2 + 2);
+    meta_size1 = * (unsigned short *) tracker;
+  	block_size1 = (meta_size1 - (meta_size1%2));
+  	meta_size2 = * (unsigned short *) (tracker +2 + block_size1);
+  	block_size2 = (meta_size2 - (meta_size2%2));
     }
-    
+
+    tracker += (2 + *(unsigned short *)tracker + block_size1);
+
+
+    }
   }
-
-*/
-
 int free1(void * ptrFree){
-  char * blockptr =  &myblock[2];
-  char * ptrFree1 = ptrFree;
-  if(ptrFree1 < &myblock[2] || ptrFree1>&myblock[4999]) 
-    return 0;
-  
+  char * ptrFree1 = (char *) ptrFree;
+  //printf("Begining of Array: %p, End of Array: %p, ptrFree: %p", &myblock[0], &myblock[4999], ptrFree);
+  if(ptrFree1 < &myblock[2] || ptrFree1>&myblock[4999]){ printf("Makes free1\n"); return 0;}
+  printf("Makes it here 22222\n");
   char * tracker = myblock;
   while(tracker<=(ptrFree1-2)){
-    if(tracker==(ptrFree1-2) && (*(short *)tracker&1)){
+    if(tracker==(ptrFree1-2) && !(*(unsigned short *)tracker%2)){
     	printf("Free makes it here\n");
-      *(short*)tracker = *(short *)tracker-1;
-      //defrag();
+      *(unsigned short*)tracker = *(unsigned short *)tracker-1;
+      defrag();
       return 1;
     }
    
-   short size =*(short *)tracker;
+   unsigned short size = *(unsigned short *)tracker;
     tracker+=(size - (size%2) +2);
   }
   
@@ -52,7 +65,7 @@ int free1(void * ptrFree){
 void * malloc1 (unsigned int size) {
   if (first == 0){
   	printf("makes it here\n");
-    *(short*) myblock = 4998;
+    *(unsigned short *) myblock = (unsigned short) 4998;
     first=1;
   }
 
@@ -62,43 +75,74 @@ void * malloc1 (unsigned int size) {
   }
   
   if(size >4998){
-    printf("Memory request larger then size of heap");
+    printf("Memory request larger then size of heap\n");
     return 0;
   }
   if(!size)size=2;
 
   if (size%2) size++;
 
-  unsigned short short_size= (unsigned short) size;
+  unsigned short short_size= size;
   char *curr = myblock;
-  short currBlocksize =0;
+  unsigned short currBlocksize =0;
 
   while(curr < &myblock[4999]){
-  if(*(short *) curr) currBlocksize = *(short*)curr &-1;
-  else{
-    printf("FUBAR\n");
-  }
+ 	currBlocksize = (*(unsigned short*)curr - (*(unsigned short *)curr%2));
+ 	//printf("Current Block Size: %u\n", currBlocksize);
+  
 
-  if (currBlocksize%2 == 0 && currBlocksize >= short_size){  //check if location is free
-          *(short*)(curr) = short_size+1;   //+1 to signify allocated space 
-          *(short*)(curr+short_size+2) = currBlocksize - short_size-2;
-          return curr+2;
+  if (*(unsigned short *) curr%2 == 0 && currBlocksize >= short_size){  //check if location is free
+          *(unsigned short *)curr = short_size+1;   //+1 to signify allocated space 
+          *(unsigned short *)(curr+short_size+2) = currBlocksize - short_size-2;
+          return (void *) curr+2;
         }
-        curr+=currBlocksize;
+        curr+=currBlocksize +2;
     }
   
   printf("Not Enough Space - 0 signifies unallocated\n");
   return NULL;
 }
 
+void testD(){
+    int count = 0;
+    void* malloc_pointers [3000];
+    int i, j;
+    srand ( time(NULL) );
+    for (i = 6000; i > 0; i--)
+    {    j =rand()%2;
+         printf("J : %d\n", j);
+         if(count==0 || (j && count!=i)){
+            malloc_pointers[count] = malloc1(rand()%6000);
+            if(malloc_pointers[count])printf("Size of malloc: %u\n", *(unsigned short *)malloc_pointers[count]);
+            count++;
+        }
+        else{
+            free1(malloc_pointers[count]);
+            printf("Free\n");
+            count--;
+        }
+        printf("Count %d: %d\n", i, count);
+}
+printf("Final Count %d\n", count);
+}
+
 int main(int argc, char *argv[]) {
 
-  char * p= malloc1(10);
-  short v = *(short*)(p-2);
-  printf("%u\n", v );
-  free1(p);
-  v = *(short *)(p-2);
-  printf("%u\n", v);
+testD();
 
+  /*char * track = myblock;
+  while(track<&myblock[4999]){
+  	unsigned short meta = *(unsigned short *)track;
+  	unsigned short blockSize = (meta - (meta%2));
+  	printf("unsi: %u\n", meta);
+  	printf("Actual size: %u", blockSize);
+  	printf("Size of meta: %u\n", meta);
+  	printf("Pointer %p\n", track);
+  	track += (2 + blockSize);
+  	//+ (*(unsigned short *)track - (*(unsigned short *)track%2));
+  	printf("Pointer %p\n", track);
+  }
+
+*/
   return 0;
 }
