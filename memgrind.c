0ae44af1776f38da8d23a15c965a1c320d268044
Andrew Khazanovich <akhazanovich+GIT@gmail.com>
2016-10-14 20:34:32 -0400

    All working time to clean up

--- memgrind.c
+++ memgrind.c
@@ -1,40 +1,91 @@
 testA(){
-    int i=00,sucess_malloc=0, success_free=0, operations = 0;
+    int i=0,sucess_malloc=0, success_free=0, operations = 0;
+    void * malloc_ptrs [3000];
     for(i=0; i<3000; i++){
-        if(malloc(1)) success_malloc++;   
+        
+        malloc_ptrs[i] = malloc(1);
+        if(malloc_ptrs[i]) success_malloc++;   
         operations++;
     }
-    char * freeTracker= array+2;   
-        
-        do{
-        if(free(freeTracker))success_free++
-        freeTracker += *((short*)(freeTracker-2))-(*((short*)(freeTracker-2))%2)+2);
-        } while(freeTracker+ *((short*)(freeTracker-2))-(*((short*)(freeTracker-2))%2)<4999)
-       
+    for(i=0;i<3000; i++){
+        if(malloc_ptrs[i]) success_free++;
+        free(malloc_ptrs[i]);
+        operations++;
+    }
         printf("Total operations: %d \n", operations);       
         printf("Successful Mallocs: %d\n", success_malloc)
         printf("Successful Frees: %d\n", success_free);
         printf("Unfreed Mallocs: %d\n", (success_malloc-success_free));
 }
-}
 
-testB{
-    void * test_B = malloc(1);
+
+testB(){
     int i = 0;
     for(; i<3000; i++){
-        free(test_B);
+       free(malloc(1))
     }
 }
 
-testC{
-    int count = 0, random, malloc_index=0, free_index=0;
+void testC(){
+    int count = 0, random, operations = 0;
     void * current_malloc;
-    char*[] malloc_pointers = new char*[3000*sizeof(char*)]
-    while(count < 6000){
-    random = rand() % 6000-count;
-    if(random%2){
-        current_malloc = malloc(1);
-        if(current_malloc)malloc_pointers[index]= current_malloc;
-        index++;
-    }
-    else
+    void* malloc_pointers [3000];
+    int i, j;
+
+
+   srand ( time(NULL) );
+    for (i = 6000; i > 0; i--)
+    {   
+        j = rand()%2;
+        printf("J: %d\n", j);
+         if(count == i){
+            free1(malloc_pointers[count]);
+            count--;
+          
+        }
+         else if(count==0){
+            malloc_pointers[count] = malloc1(1);
+            count++;
+         
+        }
+        else if(j) {
+            malloc_pointers[count] = malloc1(1);
+            count++;
+         
+        }
+        else{
+            free1(malloc_pointers[count]);
+            count--;
+            
+        }
+
+        printf("Count %d: %d\n", i, count);
+}
+printf("Final Count: %u\n", count);
+}
+
+
+void testD(){
+    int count = 0;
+    void* malloc_pointers [3000];
+    int i, j;
+    srand ( time(NULL) );
+    for (i = 6000; i > 0; i--)
+    {    j =rand()%2;
+         printf("J : %d\n", j);
+         if(count==0 || (j && count!=i)){
+            malloc_pointers[count] = malloc1(rand()%6000);
+            if(malloc_pointers[count])printf("Size of malloc: %u\n\n", *(unsigned short *)(malloc_pointers[count]-2));
+            count++;
+        }
+        else{
+            free1(malloc_pointers[count]);
+            printf("Free\n");
+            count--;
+        }
+        printf("Count %d: %d\n", i, count);
+}
+printf("Final Count %d\n", count);
+}
+
+     
